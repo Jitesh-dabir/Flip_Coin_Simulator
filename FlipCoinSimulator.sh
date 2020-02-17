b7df2d47ea2aea3daedcc2e6cf7e6e5fcb0c3a43
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-17 12:13:56 +0530

    ADDED:Check heads or tails using selection statement

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo ".............................. Welcome to Flip Coin Simulation ............................."
+
+#VARIABLE
+isFlip=1
+
+#GENERATE RANDOM NUMBER
+randomNumber=$((RANDOM%2))
+
+#CHECKING HEADS OR TAILS
+if [ $randomNumber -eq $isFlip ]
+then
+	echo "It is Heads"
+else
+	echo "It is Tails"
+fi
